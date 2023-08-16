5f4d4fc3692488682fbc1e4982f5b316cfc152be
alsafa012 <alsafa012@gmail.com>
2023-08-16 23:14:29 +0600

    PTOJECT  done USING FUMCTION

--- js/bank-using-function.js
+++ js/bank-using-function.js
@@ -16,11 +16,10 @@ function getTextFieldValue(textFieldId){
      const textField = document.getElementById(textFieldId);
      const textFieldString = textField.innerText;
      const finalTextValue = parseFloat(textFieldString);
-     textField.innerText = '';
-     if(isNaN(finalTextValue)){
-         return alert('Please enter a valid number');
-     
-     }
+     // textField.innerText = '';
+     // if(isNaN(finalTextValue)){
+     //     return alert('Please enter a valid number');   
+     // }
      return finalTextValue;
 }
 function displayValue(element, newValue){
@@ -32,33 +31,31 @@ document.getElementById('btn-deposit').addEventListener('click',function(){
      const depositAmount = getInputFieldValue('deposit-field');
      const displayTotalDeposit = getTextFieldValue('deposit-total');
      const Sum = depositAmount + displayTotalDeposit;
-     console.log(typeof depositAmount);
+     
      if(isNaN(depositAmount)){
-          return alert('Please enter a');
+          return alert('Please enter a valid deposit amount');
      }
-     console.log('Execute');
      displayValue('deposit-total',Sum);
 
      const inc = getTextFieldValue('balance-total');
      const totalIncrement = depositAmount + inc;
      
-          displayValue('balance-total', totalIncrement);
-     
-     
+     displayValue('balance-total', totalIncrement);
+
 })
-
-
 document.getElementById('btn-withdraw').addEventListener('click',function(){
      const widAmount = getInputFieldValue('withdraw-input-field');
      const displayTotalWid = getTextFieldValue('wid-calculate');
-     // const sum = x+displayTotalWid;
-     // displayValue('wid-calculate',sum);
      const dec = getTextFieldValue('balance-total');
-     if( widAmount > dec){
 
-          return alert('Failed to calculate');
-          
+     if(isNaN(widAmount)){
+     
+          return alert('Please enter a valid amount');
      }
+     if( widAmount > dec){
+          return alert('Failed to calculate');
+     }
+
      const sum = widAmount + displayTotalWid;
      displayValue('wid-calculate',sum);
 
